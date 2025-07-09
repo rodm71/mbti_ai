77aba45b1a76a283286ba6ed1a6eec95f635afcb
rodm71 <rodolphe.michel@hotmail.com>
2025-07-09 15:47:13 +0200

    Init frontend Next.js structure + page Register vide

---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {            //Composant pour la page register
+  return ( 
+    <main className="p-8">                          
+      <h1 className="text-2xl font-bold">Hello Register</h1>
+    </main>
+  )
+}
